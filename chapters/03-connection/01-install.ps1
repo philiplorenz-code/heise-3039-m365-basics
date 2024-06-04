c94a90865f895668404013c27849c8a07390848d
Philip <philip_lorenz@icloud.com>
2024-06-04 21:20:06 +0200

    added vscode extension url

--- chapters/03-connection/01-install.ps1
+++ chapters/03-connection/01-install.ps1
@@ -4,5 +4,8 @@ Install-Module -Name PnP.PowerShell -Force -AllowClobber
 # Get Module CMDLets
 Get-Command -Module PnP.PowerShell
 
-# VSCode Extension
+# VSCode Extension ID:
 adamwojcikit.pnp-powershell-extension
+
+# VSCode Extension Url:
+https://marketplace.visualstudio.com/items?itemName=adamwojcikit.pnp-powershell-extension
